7d6ddca9ac1b6a8f0cb88a18af96cb252b34d63a
Akshay-cita <edakkatakshay@gmail.com>
2020-12-11 16:30:53 +0530

    Views created

--- app1/views.py
+++ app1/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Song
+from .serializers import SongSerializer
 
-# Create your views here.
+class SongList(generics.ListCreateAPIView):
+	queryset=Song.objects.all()
+	serializer_class=SongSerializer
+
+class SongDetail(generics.RetrieveDestroyAPIView):
+	queryset=Song.objects.all()
+	serializer_class=SongSerializer
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+""" Concrete View Classes
+#CreateAPIView
+Used for create-only endpoints.
+#ListAPIView
+Used for read-only endpoints to represent a collection of model instances.
+#RetrieveAPIView
+Used for read-only endpoints to represent a single model instance.
+#DestroyAPIView
+Used for delete-only endpoints for a single model instance.
+#UpdateAPIView
+Used for update-only endpoints for a single model instance.
+##ListCreateAPIView
+Used for read-write endpoints to represent a collection of model instances.
+RetrieveUpdateAPIView
+Used for read or update endpoints to represent a single model instance.
+#RetrieveDestroyAPIView
+Used for read or delete endpoints to represent a single model instance.
+#RetrieveUpdateDestroyAPIView
+Used for read-write-delete endpoints to represent a single model instance.
+"""
