d76734da4a6b84ef7e674bf7fd819fadca5e0906
Akshay-cita <edakkatakshay@gmail.com>
2020-12-11 16:32:45 +0530

    api urls created

--- app1/urls.py
+++ app1/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 
+from .views import SongList,SongDetail
+
 
 
 urlpatterns = [
 
+	path('<int:pk>/',SongDetail.as_view(),name='songdetail'),
+	path('songlist',SongList.as_view(),name='songlist'),
 
 ]
